69ea20caa560dcc5613122fc2741157053b658dc
jayalakshmiM <jayal3393@gmail.com>
2017-06-13 15:22:09 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class odd {
+	 public static void main(String[] args)
+	    {
+		 int i;
+		 for(i=10;i<=20;i++)
+		 {
+			 if(i%2!=0)
+			 {
+				 System.out.println(i);
+			 }
+		 }
+	    }
+}
